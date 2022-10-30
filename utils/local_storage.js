4155eaa838b9d52a2cb3b7dfbfa89b4361550885
PatMiracle <miraclepatidehen@gmail.com>
2022-10-30 21:16:26 +0100

    Add utils folder

---
+++ utils/local_storage.js
@@ -0,0 +1,20 @@
+// Get Local Storage
+export function getLocalStorage(){
+    return localStorage.getItem("codeFromCodeIt")?JSON.parse(localStorage.getItem("codeFromCodeIt")):[]
+}
+
+// Add to Local Storage
+export function addToLocalStorage(html, css, js){
+    let codes = getLocalStorage()
+
+    if(codes.length < 1){
+        codes.push({html, css, js}) 
+    }
+
+    codes.map((code)=>{
+        code.html = html 
+        code.css = css
+        code.js = js
+    })
+    localStorage.setItem("codeFromCodeIt", JSON.stringify(codes))
+}
